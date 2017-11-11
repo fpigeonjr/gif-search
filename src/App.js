476f8e06cad7423d917538f9642c1ac6dad05e15
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2017-11-11 00:30:44 -0600

    adds axios and fetches data with axios

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import SearchForm from "./Components/SearchForm";
 import GifList from "./Components/GifList";
+import axios from "axios";
 
 export default class App extends Component {
   constructor() {
@@ -12,10 +13,10 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    fetch("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC")
-      .then(response => response.json())
-      .then(responseData => {
-        this.setState({ gifs: responseData.data });
+    axios
+      .get("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC")
+      .then(response => {
+        this.setState({ gifs: response.data.data });
       })
       .catch(error => {
         console.log("Error fetching and parsing data", error);
